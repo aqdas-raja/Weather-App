d840f623a8a60e0722b0508ab114b64739270d06
aqdas-raja <aqdasraja66@gmail.com>
2023-05-29 00:12:00 -0700

    Weather Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import WeatherComponent from './Components/WeatherComponent';
 
-function App() {
+
+const App = () => {
+  const [country, setCountry] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the API or perform any other logic here using the "country" state value
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+<div className='App'>
+  <div className='container'>
+    <div className='top'>
+      <div className='location'>
+        <p>Karachi</p>
+        </div>
+        <div className='temp'>
+          <h1 className='bold'>60*F</h1>
+      </div>
+      <div className='discription'>
+        <p>Clouds</p>
+      </div>
     </div>
+    <div className='bottom'>
+      <div className='feel'>
+        <p>65*F</p>
+      </div>
+      <div className='humidity'>
+        <p className='bold'>20%</p>
+        <p>20%</p>
+        <p>Hmidity</p>
+      </div>
+      <div className='wind'>
+        <p className='bold'>12 MPH</p>
+        <p>Wind SpeedH</p>
+      </div>
+    </div>
+  </div>
+</div>
+    
   );
-}
+};
 
 export default App;
